7e623269ce11a293d54db192bc4684884407c499
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-24 02:00:45 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { useState ,useEffect} from 'react';
-
+import React, { useState ,useEffect} from 'react'; //useEffect import
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+//Remember this takes in two parameters, the first is the function to be executed,
+// and the second is the dependency array, and it will check if that changes and if it does
+    // then it will execute the function again
+    useEffect(() => {
 
+        if (localStorage.getItem('isLoggedIn') === '1') {
+            setIsLoggedIn(true);
+        }
+
+    }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
@@ -13,16 +21,6 @@ function App() {
       localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
-//Remember this takes in two parameters, the first is the function to be executed,
-// and the second is the dependency array, and it will check if that changes and if it does
-    // then it will execute the function again
-  useEffect(() => {
-
-      if (localStorage.getItem('isLoggedIn') === '1') {
-        setIsLoggedIn(true);
-      }
-
-  }, []);
   const logoutHandler = () => {
     setIsLoggedIn(false);
   };
